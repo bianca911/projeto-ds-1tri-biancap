7774cb59a799784b89bbdfbd879189483f9e54f1
gigioo-00 <giovannagabriellyy001@gmail.com>
2026-04-08 08:56:50 -0300

    adicionei o campo para selecionar a credencial e ajustei o style dos textos

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,19 +1,43 @@
-
-import { StyleSheet, ScrollView } from 'react-native';
+import React, { useState } from 'react'; // Importado useState
+import { StyleSheet, ScrollView, View, TextInput, TouchableOpacity, Text } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
-import { View } from 'react-native';
-
 
 export default function HomeScreen() {
+  const [credencial, setCredencial] = useState(''); //valor da credencial
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
+
+      <View style={styles.centralizar}>
+        <ThemedText type="subtitle" style={styles.subtituloCustomizado}>
+          Colégio
+        </ThemedText>
+      </View>
+
+      <View style={styles.centralizar}>
+        <ThemedText type="title" style={styles.tituloCustomizado}>
+          Luláticos
+        </ThemedText>
+      </View>
+
+      <ThemedText type="subtitle" style={styles.credencialtexto}>
+        insira sua credencial de acesso
+      </ThemedText>
+
       
-      <View style={styles.centralizar}>
-        <ThemedText type="subtitle" style={styles.subtituloCustomizado}>Colégio</ThemedText>
-      </View>
-      <View style={styles.centralizar}>
-        <ThemedText type="title" style={styles.tituloCustomizado}>Lunáticos</ThemedText>
-      </View>
+      <TextInput
+        style={styles.input}
+        value={credencial}
+        onChangeText={(text) => setCredencial(text.replace(/[^0-9]/g, ''))} //deixar só digitar números
+        keyboardType="numeric"
+        autoCapitalize="none"
+      />
+
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={() => console.log('Acessando com:', credencial)}
+      >
+        <Text style={styles.textoBotao}>continuar</Text>
+      </TouchableOpacity>
 
     </ScrollView>
   );
@@ -22,20 +46,57 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
   },
   subtituloCustomizado: {
     color: '#000',
-    fontSize: 48,
+    fontSize: 40,
     fontFamily: 'Itim',
   },
   tituloCustomizado: {
     color: '#FFC567',
     fontSize: 70,
     fontFamily: 'MuseoBold',
+    marginBottom: 100,
   },
   centralizar: {
-    flex: 1,
+    marginVertical: 5,
+  },
+  credencialtexto: {
+    color: '#000',
+    fontSize: 15,
+    fontFamily: 'Itim',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  
+  input: {
+    width: '90%',
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#000',
+    borderRadius: 20, 
+    paddingHorizontal: 20,
+    fontSize: 15,
+    fontFamily: 'Itim',
+    marginBottom: 15,
+  },
+  botao: {
+    width: '90%',
+    height: 50,
+    backgroundColor: '#FFC567',
+    borderWidth: 2,
+    borderColor: '#000',
+    borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  textoBotao: {
+    fontSize: 15,
+    fontFamily: 'Itim',
+    color: '#000',
+  }
 });
